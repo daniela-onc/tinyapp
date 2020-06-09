be56faa65b79ea1c86f4c8fe9dd94a3e67a02ed9
Daniela <danielaoancea2009@yahoo.ca>
2020-06-09 03:35:57 +0000

    URL Shortening (Part 1) Forms

--- express_server.js
+++ express_server.js
@@ -8,6 +8,13 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+  
+
+const bodyParser = require("body-parser"); //this should be declared before all of the routes
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -27,7 +34,28 @@ app.get("/urls", (req, res) => {
 });
 
 
-app.get("/urls/:shortURL", (req, res) => {
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+//generate a Random ShortURL
+function generateRandomString() {
+let randomCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+let randomNumber = "";
+for(var i = 0; i < 6; i++) {
+  randomNumber += randomCharacters.charAt(Math.floor(Math.random()*randomCharacters.length));
+}
+return randomNumber;
+}
+
+
+
+app.get("/urls/new", (req, res) => { 
+  res.render("urls_new"); //GET Route to Show the Form to the User
+});
+
+app.get("/urls/:shortURL", (req, res) => { //route definition
   let shortURLName = req.params.shortURL;
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[shortURLName]}; /* What goes here?*/
   res.render("urls_show", templateVars);
